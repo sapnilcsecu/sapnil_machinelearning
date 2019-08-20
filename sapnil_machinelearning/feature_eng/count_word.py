8ff996dae548062c72ce7d13c0e8b2172e0132d8
rmisys123 <sapnil123@yahoo.com>
2019-08-21 02:49:41 +0600

    add the multi_nativebayes

--- sapnil_machinelearning/feature_eng/count_word.py
+++ sapnil_machinelearning/feature_eng/count_word.py
@@ -14,7 +14,7 @@ def count_word_fit(doc_list,class_labels):
     
     
      #Using Python's stop-words package to get the stop words in English
-   
+    '''
     stop_words = get_stop_words('english')
     for doc in doc_list:
        words = doc.split(" ")
@@ -23,18 +23,38 @@ def count_word_fit(doc_list,class_labels):
             vocabularySet.add(word)
             vocabulary.append(word)
             vocabularyCount = vocabularyCount + 1
-            
+      '''   
+    
+   
     
     #class_labels = list(dict.fromkeys(class_labels))    
     
     #class_eachtoken_count = [[0 for x in range(vocabularyCount)] for y in range(len(class_labels))]    
     #class_eachtoken_count=nd.nested_dict()
    
+    class_labels = list(dict.fromkeys(class_labels))
     class_eachtoken_count = {}
+    total_class_token={}
+    '''
     for class_label in class_labels: 
         for voc in vocabulary:
             class_eachtoken_count[class_label] [voc]=0
+     '''       
+    doc_count=0;        
+    stop_words = get_stop_words('english')
+    for doc in doc_list:
+        words = doc.split(" ")
+        for word in words:                        
+            if word not in vocabularySet and word not in stop_words:
+                vocabularySet.add(word)
+                vocabulary.append(word)
+                label=class_labels[doc_count]
+                ++class_eachtoken_count[label][word] 
+                ++total_class_token[doc_count] 
+                vocabularyCount = vocabularyCount + 1   
+        ++doc_count 
     
+    '''
     doc_count=0
     for doc in doc_list:
         words = doc.split(" ");
@@ -42,7 +62,9 @@ def count_word_fit(doc_list,class_labels):
             if word in vocabularySet:
                 label=class_labels[doc_count]
                 ++class_eachtoken_count[label][word] 
+                ++total_class_token[doc_count] 
+               
         ++doc_count 
-              
+    '''          
             
-    return vocabularyCount
+    return vocabularyCount,class_eachtoken_count,total_class_token,class_labels,vocabulary
