c4c214fdd522d39e6afd79b1a25a19fcdd8d15e6
nasircsecu <nasircsecu@gmail.com>
2019-08-21 17:58:39 +0600

    work on count word function

--- sapnil_machinelearning/classifier/multinomial_nativebayes.py
+++ sapnil_machinelearning/classifier/multinomial_nativebayes.py
@@ -7,20 +7,21 @@ import math
 
 def multi_nativebayes(vocabularyCount,class_eachtoken_count,total_class_token,class_labels,vocabulary):
     condProbabilityOfTermClass={}
+    '''
+    for class_label in class_labels:
+        for word in vocabulary:
+            condProbabilityOfTermClass[class_label] =condProbabilityOfTermClass[class_label]+math.log((class_eachtoken_count[class_label][word]+1)/(total_class_token[class_label]+1))
+    
+    '''
+    scoreClass =0
+    label_count=0
+    max_score=0
     
     for class_label in class_labels:
         for word in vocabulary:
-           condProbabilityOfTermClass[class_label] =condProbabilityOfTermClass[class_label]+math.log((class_eachtoken_count[class_label][word]+1)/(total_class_token[class_label]+1))
-        
-    
-    scoreClass = [0 for x in range(5)]
-    label_count=0;
-    max_score=0;
-    
-    for class_label in class_labels:
-        scoreClass[label_count] = math.log(total_class_token[label_count]/vocabularyCount) + math.log(condProbabilityOfTermClass[class_label])
-        if(max_score<scoreClass[label_count]):
-            max_score=scoreClass[label_count]
-            final_class_label=class_label
+            scoreClass = math.log(total_class_token[label_count]/vocabularyCount) + math.log((class_eachtoken_count[class_label][word]+1)/(total_class_token[class_label]+1))
+            if(max_score<scoreClass):
+                max_score=scoreClass
+                final_class_label=class_label
         ++label_count
     return final_class_label
---
+++ sapnil_machinelearning/example/multinomial_example.py
@@ -0,0 +1,19 @@
+'''
+Created on Aug 21, 2019
+
+@author: Nasir uddin
+'''
+
+from dataset_pre.dataset_load import load_cvs_dataset
+from dataset_pre.dataset_load import splitDataset
+from feature_eng.count_word import count_word_fit
+
+def main():
+    trainDF = load_cvs_dataset("../corpus.csv")
+    txt_label = trainDF['label']
+    txt_text = trainDF['text']
+    trainSet, testcopy, labelset, testlabelcopy=splitDataset(txt_text, txt_label,0.2)
+    count_word_fit(trainSet,labelset)
+    
+if __name__ == '__main__':
+    main()
