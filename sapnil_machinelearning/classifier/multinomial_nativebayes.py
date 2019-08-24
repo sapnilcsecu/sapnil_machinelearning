ab89c492c2369649e49aebe633e3ac2d0e7b417f
rmisys123 <sapnil123@yahoo.com>
2019-08-25 02:58:05 +0600

    complete the prediction function

--- sapnil_machinelearning/classifier/multinomial_nativebayes.py
+++ sapnil_machinelearning/classifier/multinomial_nativebayes.py
@@ -4,32 +4,42 @@ Created on Aug 7, 2019
 @author: Nasir(programmer)
 '''
 import math
+from nltk.tokenize import word_tokenize
 
 
 
-
-def multi_nativebayes_predict(self,model_data):
+def multi_nativebayes_predict(self,model_data,test_dataset,testlabelcopy):
     condProbabilityOfTermClass={}
-    for class_label in model_data.get_class_labels(): 
-        condProbabilityOfTermClass[class_label]=0
-    for class_label in model_data.get_class_labels():
-        total_class_token=model_data.get_total_class_token()
-        for word in model_data.get_vocabulary():
-            class_eachtoken_count=model_data.get_class_eachtoken_count()
-            
-            condProbabilityOfTermClass[class_label] =condProbabilityOfTermClass[class_label]+math.log((class_eachtoken_count[class_label][word]+1)/(total_class_token[class_label]+1))
-    
-    
-    scoreClass =0
-    label_count=0
-    max_score=0
-    vocabularyCount=model_data.get_vocabularyCount()
-    for class_label in model_data.get_class_labels:
-        total_class_token=model_data.get_total_class_token()
-        for word in model_data.get_vocabulary():
-            scoreClass = math.log(total_class_token/vocabularyCount) + condProbabilityOfTermClass[class_label]
-            if(max_score<scoreClass):
-                max_score=scoreClass
-                final_class_label=class_label
-        ++label_count
-    return final_class_label
+    final_doc_class_label={}
+    doccount=0;
+    for doc in test_dataset:
+        final_doc_class_label[doc]=0
+        words = word_tokenize(doc)
+        for class_label in model_data.get_class_labels(): 
+            condProbabilityOfTermClass[class_label]=0
+            total_class_token=model_data.get_total_class_token()
+            for word in words:
+                class_eachtoken_count=model_data.get_class_eachtoken_count()
+                if(class_eachtoken_count[class_label][word]):
+                    condProbabilityOfTermClass[class_label] =condProbabilityOfTermClass[class_label]+math.log((class_eachtoken_count[class_label][word]+1)/(total_class_token[class_label]+1))
+                else:
+                    condProbabilityOfTermClass[class_label] =condProbabilityOfTermClass[class_label]+math.log(1/(total_class_token[class_label]+1))
+        
+        score_Class =0
+        max_score=0
+        final_class_label=''
+       
+        vocabularyCount=model_data.get_vocabularyCount()
+        for class_label in model_data.get_class_labels:
+            total_class_token=model_data.get_total_class_token()
+            for word in model_data.get_vocabulary():
+                score_Class = math.log(total_class_token/vocabularyCount) + condProbabilityOfTermClass[class_label]
+                if(max_score<score_Class):
+                    max_score=score_Class
+                    final_class_label=class_label
+        final_doc_class_label[doc]=final_class_label
+        
+        ++doccount            
+                    
+        
+    return final_doc_class_label
--- sapnil_machinelearning/example/multinomial_example.py
+++ sapnil_machinelearning/example/multinomial_example.py
@@ -7,7 +7,7 @@ Created on Aug 21, 2019
 from dataset_pre.dataset_load import load_cvs_dataset
 from dataset_pre.dataset_load import splitDataset
 from feature_eng.count_word import count_word_fit
-from classifier.multinomial_nativebayes import multi_nativebayes_fit
+from classifier.multinomial_nativebayes import multi_nativebayes_predict
 
 def main():
     trainDF = load_cvs_dataset("../corpus.csv")
@@ -16,7 +16,7 @@ def main():
     trainSet, testcopy, labelset, testlabelcopy=splitDataset(txt_text, txt_label,0.2)
     model_input=count_word_fit(trainSet,labelset)
 
-    multi_nativebayes_fit(model_input[0],model_input[1],model_input[2],model_input[3],model_input[4])
+    multi_nativebayes_predict(model_input)
     
 if __name__ == '__main__':
     main()
